05b32f7aa823bab41c5ca3cac0f023fef98d0388
Eureka <747417582@qq.com>
2022-12-11 14:52:08 +0800

    square

--- miniprogram/pages/square/square.js
+++ miniprogram/pages/square/square.js
@@ -1,39 +1,74 @@
 // pages/square/square.js
+import { createStoreBindings } from 'mobx-miniprogram-bindings'
+import { store } from '../../store/store'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        stores:[
-            {
-                id: 1,
-                name: 'KFC',
-                price: 50
-            },
-            {
-                id: 2,
-                name: 'KFC',
-                price: 50
-            },
-            {
-                id: 3,
-                name: 'KFC',
-                price: 50
-            },
-            {
-                id: 4,
-                name: 'KFC',
-                price: 50
-            },
-            {
-                id: 5,
-                name: 'KFC',
-                price: 50
-            },
-        ]
+        defalutLiked:false,
+        info: {
+            
+            user_name: "阿尼亚",
+            comment: []
+        },
     },
+    getComments(){
+        wx.request({
+          url: 'https://mock.apifox.cn/m1/1961063-0-default/user/',
+          method:"GET",
+          success:(res)=>{
+            // console.log(res);
+            res.data.comment.forEach((comment)=>{
+                comment.isLiked = this.data.defalutLiked
+            })
+            console.log(res.data.comment);
+            this.setData({
+                "info.comment":[...this.data.info.comment,...res.data.comment],
+            })
+          },
+          complete:()=>{
+              wx.hideLoading({
+                success: (res) => {},
+              })
+          }
+        })
+        
 
+    },
+    onMyEvent: function (event) {
+        if (!this.data.isLogined) {
+            wx.navigateTo({
+              url: '../login/login',
+            })
+            return
+        }
+        // console.log(event);
+        const this_comment_Index = this.data.info.comment.findIndex((item) =>
+            item.comment_id == event.detail.this_id
+        )
+        // console.log(this_comment);
+        console.log(this_comment_Index);
+        if (this.data.info.comment[this_comment_Index].isLiked) 
+        {
+            this.setData({
+                [`info.comment[${this_comment_Index}].isLiked`]: false,
+                [`info.comment[${this_comment_Index}].comment_like_count`]:this.data.info.comment[this_comment_Index].comment_like_count - 1
+            });
+            // this_comment.isLiked=false;
+        } else 
+        {
+            this.setData({
+                [`info.comment[${this_comment_Index}].isLiked`]: true,
+                [`info.comment[${this_comment_Index}].comment_like_count`]:this.data.info.comment[this_comment_Index].comment_like_count + 1
+            });
+            // this_comment.isLiked=true;
+        }
+        // console.log(this_comment);
+        // console.log(this.data.info);
+    },
+    
     // 页面刷新请求数据
     getData(){
     wx.request({
@@ -63,7 +98,12 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        
+        this.getComments();
+        this.storeBindings = createStoreBindings(this,{
+            store,
+            fields: ['isLogined'],
+            actions: ['login'],
+        })
     },
 
     /**
